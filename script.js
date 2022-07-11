0c2fbc927758d82989ee56eb26c836dc61993673
Chris Ko <ko_ka_chun@hotmail.com>
2022-07-11 12:57:36 +0100

    created an object for storing morse code for each letter and identified classes for different elements.

---
+++ script.js
@@ -0,0 +1,39 @@
+// handle user input
+
+// do translation
+// create an obj to store morse code for each letter
+// loop through the input words or sentences, and return a new array which stores the translated letter
+// join the new array and return it
+
+// return an ouput
+
+const morseCode = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+}
+
+
+
